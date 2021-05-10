61a6d62edb723e0394ee3a6f0cb2e30abf7ed51e
Eduardo Baptista <eduardofbaptista@gmail.com>
2021-05-09 22:45:08 -0300

    Update methods to calculate reports

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -17,51 +17,28 @@ defmodule GenReport do
            "hours_per_year" => hours_per_year
          }
        ) do
-    all_hours = update_all_hours_report(all_hours, name, hours)
-    hours_per_month = update_hours_per_month_report(hours_per_month, name, month, hours)
-    hours_per_year = update_hours_per_year_report(hours_per_year, name, year, hours)
+    all_hours = update_total_value(all_hours, name, hours)
+    hours_per_month = update_map(hours_per_month, name, month, hours)
+    hours_per_year = update_map(hours_per_year, name, year, hours)
     build_report(all_hours, hours_per_month, hours_per_year)
   end
 
-  defp update_all_hours_report(all_hours, name, hours) do
-    total_hours = get_safe_value(all_hours, name)
-    Map.put(all_hours, name, hours + total_hours)
+  defp update_total_value(map, key, hours) do
+    total_hours = Map.get(map, key, 0)
+    Map.put(map, key, hours + total_hours)
   end
 
-  defp update_hours_per_month_report(
-         hours_per_month,
-         name,
-         month,
-         hours
-       ) do
-    report = get_safe_map(hours_per_month, name)
-    total_hours = get_safe_value(report, month)
-    report = Map.put(report, month, hours + total_hours)
-    Map.put(hours_per_month, name, report)
+  defp update_map(map, name, key, hours) do
+    report =
+      map
+      |> Map.get(name, %{})
+      |> update_total_value(key, hours)
+
+    Map.put(map, name, report)
   end
-
-  defp update_hours_per_year_report(
-         hours_per_year,
-         name,
-         year,
-         hours
-       ) do
-    report = get_safe_map(hours_per_year, name)
-    total_hours = get_safe_value(report, year)
-    report = Map.put(report, year, hours + total_hours)
-    Map.put(hours_per_year, name, report)
-  end
-
-  defp get_safe_map(map, key), do: Map.get(map, key, %{})
-
-  defp get_safe_value(map, key), do: Map.get(map, key, 0)
 
   defp create_empty_report do
-    %{
-      "all_hours" => %{},
-      "hours_per_month" => %{},
-      "hours_per_year" => %{}
-    }
+    %{"all_hours" => %{}, "hours_per_month" => %{}, "hours_per_year" => %{}}
   end
 
   defp build_report(all_hours, hours_per_month, hours_per_year) do
